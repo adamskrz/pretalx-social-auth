f1bee8025c8c61ab3dcb8f5cabab3782ba3cc876
Adam Skrzymowski <35405850+adamskrz@users.noreply.github.com>
2024-12-29 20:49:09 +0100

    Initial test with auth form signal

--- pretalx_social_auth/signals.py
+++ pretalx_social_auth/signals.py
@@ -1,8 +1,10 @@
 
 from django.dispatch import receiver
 from django.urls import reverse
+from django.template.loader import get_template
 
 from pretalx.orga.signals import nav_event_settings
+from pretalx.common.signals import auth_html
 
 
 @receiver(nav_event_settings)
@@ -21,3 +23,8 @@ def pretalx_social_auth_settings(sender, request, **kwargs):
         }
     ]
 
+@receiver(auth_html)
+def render_login_auth_options(sender, **kwargs):
+    template = get_template("pretalx_social_auth/login.html")
+    html = template.render()
+    return html
