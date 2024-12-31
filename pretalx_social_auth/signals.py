730e3dcd53851bcd6d9872085b9b388eca67f0f0
Adam Skrzymowski <35405850+adamskrz@users.noreply.github.com>
2024-12-31 21:51:00 +0100

    Added backend friendly names

--- pretalx_social_auth/signals.py
+++ pretalx_social_auth/signals.py
@@ -5,7 +5,7 @@ from pretalx.common.signals import auth_html
 from pretalx.orga.signals import nav_event_settings
 from pretalx.person.signals import deactivate_user
 
-from .utils import backends
+from .utils import all_backends, backend_friendly_name
 
 
 @receiver(nav_event_settings)
@@ -28,8 +28,12 @@ def pretalx_social_auth_settings(sender, request, **kwargs):
 @receiver(auth_html)
 def render_login_auth_options(sender, request, next_url=None, **kwargs):
     print("render_login_auth_options")
-    context = backends(request)
+    context = {}
     context["url_params"] = ""
+    context["backends"] = {
+        class_name: backend_friendly_name(be_class)
+        for class_name, be_class in all_backends().items()
+    }
 
     next_path = request.GET.get("next", next_url)
     if next_path:
